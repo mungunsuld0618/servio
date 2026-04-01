f1d3a7c7200c96fe32415c5ec582d4d0fda32c4f
mungunsuld0618 <mungunsuldb@gmail.com>
2026-04-01 15:55:09 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-          remotePatterns: [
-            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-            { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-            { protocol: 'https', hostname: 'res.cloudinary.com' },
-            { protocol: 'https', hostname: '**' },
-                ],
-    },
-    experimental: {
-          serverActions: true,
-    },
-    webpack: (config) => {
-          config.resolve.alias = {
-                  ...config.resolve.alias,
-                  '@': process.cwd(),
-          };
-          return config;
-    },
+  reactStrictMode: true,
+  swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+      { protocol: 'https', hostname: 'res.cloudinary.com' },
+      { protocol: 'https', hostname: '**' },
+    ],
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': process.cwd(),
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
