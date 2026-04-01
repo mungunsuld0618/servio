4e066ed06ccc6090fab684520ba926a62675ea51
mungunsuld0618 <mungunsuldb@gmail.com>
2026-04-01 15:34:28 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,31 +1,25 @@
-import withSerwist from '@serwist/next';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
-      { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
-      { protocol: 'https', hostname: 'res.cloudinary.com' },
-      { protocol: 'https', hostname: '**' },
-    ],
-  },
-  experimental: {
-    serverActions: true,
-  },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': process.cwd(),
-    };
-    return config;
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    images: {
+          remotePatterns: [
+            { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+            { protocol: 'https', hostname: 'avatars.githubusercontent.com' },
+            { protocol: 'https', hostname: 'res.cloudinary.com' },
+            { protocol: 'https', hostname: '**' },
+                ],
+    },
+    experimental: {
+          serverActions: true,
+    },
+    webpack: (config) => {
+          config.resolve.alias = {
+                  ...config.resolve.alias,
+                  '@': process.cwd(),
+          };
+          return config;
+    },
 };
 
-export default withSerwist({
-  ...nextConfig,
-  swSrc: 'app/sw.ts',
-  swDest: 'public/sw.js',
-});
+export default nextConfig;
